835d04a8bf9cdd6fcfc23562e4ab81f27455bdd5
Kate Eggenberg <keggenb@jacksonville.edu>
2026-02-10 16:06:43 -0500

    Added CRUD Pages for RequestAttachments, RequestComments, UserRoles, and Fixed the Progran.cs file so that it calls the proper connection string.

---
+++ CampusConnect/Pages/RequestAttachmentsPages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CampusConnect.Data;
+using CampusConnect.Models;
+
+namespace CampusConnect.Pages.RequestAttachmentsPages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly CampusConnect.Data.TablesDbContext _context;
+
+        public IndexModel(CampusConnect.Data.TablesDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<attachments> attachments { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            attachments = await _context.attachments
+                .Include(a => a.creator)
+                .Include(a => a.request).ToListAsync();
+        }
+    }
+}
