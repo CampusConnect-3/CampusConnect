c7d0203ed00f52be0632f496c3a37601eb66fd9f
Kate <71048152+k8marxdaspot@users.noreply.github.com>
2026-02-24 09:23:34 -0500

    fixed authroization and duplicate naming bug. should run now without build errors

--- CampusConnect/Pages/RequestAttachmentsPages/Delete.cshtml.cs
+++ CampusConnect/Pages/RequestAttachmentsPages/Delete.cshtml.cs
@@ -8,11 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using CampusConnect.Data;
-using CampusConnect.Models;
 using Microsoft.Extensions.Logging;
 using System.Security.Claims;
 
